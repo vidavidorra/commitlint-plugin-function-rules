1f38e867813ecf7e8e70e4d4d0d03295f0b63907
Jeroen de Bruijn <vidavidorra+jdbruijn@gmail.com>
2020-09-06 09:32:21 +0200

    chore: setup repository for TypeScript

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,5 @@
 const defaultConfig = {
-  extends: [
-    'eslint:recommended',
-    'plugin:json/recommended',
-    'plugin:prettier/recommended',
-  ],
+  extends: ['eslint:recommended', 'plugin:prettier/recommended'],
   plugins: [],
   parserOptions: {
     sourceType: 'module',
@@ -13,27 +9,74 @@ const defaultConfig = {
     node: true,
   },
   rules: {
+    'no-restricted-globals': [
+      'error',
+      /**
+       * From Jest global environment.
+       * https://github.com/facebook/jest/blob/v26.4.2/packages/jest-globals/src/index.ts#L12-L27
+       */
+      'jest',
+      'expect',
+      'it',
+      'test',
+      'fit',
+      'xit',
+      'xtest',
+      'describe',
+      'xdescribe',
+      'fdescribe',
+      'beforeAll',
+      'beforeEach',
+      'afterEach',
+      'afterAll',
+    ],
     'prettier/prettier': 'error',
     'sort-imports': 'error',
   },
 };
 
+/**
+ * Add to `extends` of `defaultConfig`.
+ *
+ * The Prettier recommended configuration must be the last extension. See
+ * https://github.com/prettier/eslint-plugin-prettier#recommended-configuration.
+ *
+ * @param  {...configurations} configurations Configuration(s) to add.
+ * @return Superset of the `extends` with the given configuration(s) added.
+ */
+function addToDefaultExtends(...configurations) {
+  const prettierConfiguration = 'plugin:prettier/recommended';
+  const extendsSuperset = defaultConfig.extends
+    .concat(configurations)
+    .sort((a, b) => {
+      if (b === prettierConfiguration) {
+        return -1;
+      }
+      if (a === prettierConfiguration) {
+        return 1;
+      }
+      return 0;
+    });
+
+  return extendsSuperset;
+}
+
 const config = defaultConfig;
 config.overrides = [
   {
     files: ['**/*.ts', '**/*.tsx'],
-    /**
-     * The Prettier recommended configuration must remain the configuration that
-     * is extended last. Therefore, this overrides `extends` completely.
-     */
-    extends: [
-      'eslint:recommended',
+    extends: addToDefaultExtends(
       'plugin:@typescript-eslint/recommended',
       'prettier/@typescript-eslint',
-      'plugin:prettier/recommended',
-    ],
+      'plugin:jest/style',
+    ),
     parser: '@typescript-eslint/parser',
     plugins: defaultConfig.plugins.concat(['@typescript-eslint']),
+    rules: { ...defaultConfig.rules, 'jest/consistent-test-it': 'error' },
+  },
+  {
+    files: ['**/*.json'],
+    extends: addToDefaultExtends('plugin:json/recommended'),
   },
 ];
 
---
+++ jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  collectCoverage: true,
+  collectCoverageFrom: ['src/**/*.ts'],
+  coverageDirectory: 'coverage',
+  coverageReporters: ['cobertura', 'lcov', 'text'],
+  preset: 'ts-jest',
+  roots: ['<rootDir>/src'],
+};
