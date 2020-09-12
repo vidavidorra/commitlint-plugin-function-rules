2df8c0e4eb4f3a80d32ff36f617f39108081b735
Jeroen de Bruijn <vidavidorra+jdbruijn@gmail.com>
2020-09-12 18:57:18 +0200

    chore: run tests on staged files using lint-staged

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,5 +1,9 @@
 module.exports = {
-  '*.{ts,tsx,js,jsx,json}': ['npm run format:file', 'npm run lint-es:file:fix'],
+  '*.{ts,tsx,js,jsx,json}': [
+    'npm run format:file',
+    'npm run lint-es:file:fix',
+    'npm run test:staged',
+  ],
   '*.{vue,css,less,scss,html,htm,md,markdown,yml,yaml}': [
     'npm run format:file',
   ],
