9d7715d9355483901906582d0cd002b2ffa3b00a
Jeroen de Bruijn <vidavidorra+jdbruijn@gmail.com>
2020-09-19 21:43:56 +0200

    test(end-to-end): test agains commitlint v9.x, v10.x and v11.x
    This is to increase support for multiple commitlint versions in the future and keep track of potentially breaking changes in all supported versions.

--- test/end-to-end.test.ts
+++ test/end-to-end.test.ts
@@ -1,49 +1,75 @@
 import * as plugin from '../src';
-import { Plugin, QualifiedRules, RuleOutcome } from '@commitlint/types';
-import { describe, expect, it } from '@jest/globals';
-import lint from '@commitlint/lint';
-import load from '@commitlint/load';
+import { beforeAll, describe, expect, it } from '@jest/globals';
+import { RuleOutcome } from '@commitlint/types';
 
 describe('commitlint plugin function rules', () => {
-  it('can be loaded using @commitlint/load', async () => {
-    await expect(
-      load({
-        plugins: [plugin as Plugin],
-      }),
-    ).resolves.not.toThrow();
-  });
+  /**
+   * Due to a but in the `describe.each` type, it is not possible to use it with
+   * `@jest/globals`. See https://github.com/facebook/jest/issues/10447 for
+   * the specific issue. As a workaround the TypeScript error is ignored.
+   */
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-expect-error: TS2349: This expression is not callable.
+  describe.each(['9.x', '10.x', '11.x'])(
+    'commitlint v%s',
+    (version: string) => {
+      /* eslint-disable @typescript-eslint/no-explicit-any */
+      let load: any;
+      let lint: any;
+      /* eslint-enable @typescript-eslint/no-explicit-any */
 
-  it('provides rules that can be used with commitlint', async () => {
-    for await (const rule of Object.keys(plugin.rules)) {
-      const results: RuleOutcome[] = [
-        [true],
-        [false, `error message from ${rule}`],
-      ];
+      beforeAll(async () => {
+        load = await (await import(`@commitlint/load-${version}`)).default;
+        expect(load).not.toBeUndefined();
 
-      for await (const result of results) {
-        const rules: Partial<QualifiedRules> = {};
-        rules[rule] = [
-          2,
-          'always',
-          () => {
-            return result;
-          },
-        ];
+        lint = await (await import(`@commitlint/lint-${version}`)).default;
+        expect(lint).not.toBeUndefined();
+      });
 
-        const report = await lint('chore: basic commit message', rules, {
-          plugins: {
-            'function-rules': plugin as Plugin,
-          },
-        });
+      it('@commitlint/load can load the plugin', async () => {
+        await expect(
+          load({
+            plugins: [plugin],
+          }),
+        ).resolves.not.toThrow();
+      });
 
-        expect(report.valid).toEqual(result[0]);
-        if (result[1] !== undefined) {
-          expect(report.warnings).toHaveLength(0);
-          expect(report.errors).toHaveLength(1);
-          expect(report.errors[0].name).toEqual(rule);
-          expect(report.errors[0].message).toEqual(result[1]);
+      it('@commitlint/lint can use the plugin rules', async () => {
+        for await (const rule of Object.keys(plugin.rules)) {
+          const results: RuleOutcome[] = [
+            [true],
+            [false, `error message from ${rule}`],
+          ];
+
+          for await (const result of results) {
+            const report = await lint(
+              'chore: basic commit message',
+              {
+                [rule]: [
+                  2,
+                  'always',
+                  () => {
+                    return result;
+                  },
+                ],
+              },
+              {
+                plugins: {
+                  'function-rules': plugin,
+                },
+              },
+            );
+
+            expect(report.valid).toEqual(result[0]);
+            if (result[1] !== undefined) {
+              expect(report.warnings).toHaveLength(0);
+              expect(report.errors).toHaveLength(1);
+              expect(report.errors[0].name).toEqual(rule);
+              expect(report.errors[0].message).toEqual(result[1]);
+            }
+          }
         }
-      }
-    }
-  });
+      });
+    },
+  );
 });
