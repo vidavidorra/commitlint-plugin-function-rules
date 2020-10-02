c772b9919c17b92115f4ed76bf370e898f2e859f
Jeroen de Bruijn <vv9239@gmail.com>
2020-10-02 17:37:48 +0200

    ci: run full tests in lint-staged
    Full tests only take a couple of seconds, so time penalty is minimal IMO. Full tests prevent stupid failures.

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -2,7 +2,7 @@ module.exports = {
   '*.{ts,tsx,js,jsx,json}': [
     'npm run format:file',
     'npm run lint-es:file:fix',
-    'npm run test:staged',
+    () => 'npm run test',
   ],
   '*.{vue,css,less,scss,html,htm,md,markdown,yml,yaml}': [
     'npm run format:file',
