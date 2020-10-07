30c5dc4a895383a04e058cda807cdab741ca2904
Jeroen de Bruijn <vv9239@gmail.com>
2020-10-07 20:27:49 +0200

    ci: separate build check from lint and check build pre-commit

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -2,6 +2,7 @@ module.exports = {
   '*.{ts,tsx,js,jsx,json}': [
     'npm run format:file',
     'npm run lint-es:file:fix',
+    () => 'npm run build:check',
     () => 'npm run test',
   ],
   '*.{vue,css,less,scss,html,htm,md,markdown,yml,yaml}': [
