c29c41db1db30c4374165140a8b6fe766da2419a
Jeroen de Bruijn <vv9239@gmail.com>
2023-11-06 20:03:17 +0100

    style: format `lint-staged.js`

--- .github/lint-staged.js
+++ .github/lint-staged.js
@@ -1,10 +1,6 @@
 const config = {
-  '*.{ts,tsx,js,jsx}': [
-    'xo --fix',
-    () => 'ava'
-  ],
+  '*.{ts,tsx,js,jsx}': ['xo --fix', () => 'ava'],
   '*.{vue,css,less,scss,html,htm,json,md,markdown,yml,yaml}':
     'prettier --write',
 };
-
 export default config;
